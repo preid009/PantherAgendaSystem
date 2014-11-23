780887159904620cc8573271755d6ca721c5b2bd
Pete1014 <preid009@fiu.edu>
2014-11-23 13:21:56 -0500

    Signatures added

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Peter
+ * Date: 11/23/2014
+ * Time: 12:15 PM
+ */
+
+class UsersController extends AppController{
+
+    /**
+    Adds a new user to the system.
+    return: boolean: True if the user is added.
+     */
+    public function add(){
+
+    }
+
+    /**
+    Logs in a user.
+    return: boolean: True if the user is logged in.
+     */
+    public function login(){
+
+    }
+
+    /**
+    Logs out a user.
+    return: boolean: True if the user is logged out.
+     */
+    public function logout(){
+
+    }
+ /**
+ Locks out a user.
+ return: boolean: True if the user is locked out.
+  */
+    public function lockout(){
+
+    }
+}
---
+++ app/Model/User.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Peter
+ * Date: 11/23/2014
+ * Time: 12:06 PM
+ */
+
+class User extends AppModel{
+
+}
